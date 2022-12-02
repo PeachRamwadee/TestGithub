ace923622b2778dca873e8e49417a21d78fd5a87
ramwadeeketkan <Ramwadee.k@gmail.com>
2022-12-02 15:41:03 +0700

    test github

--- app/src/main/java/com/dev/testgithub/MainActivity.kt
+++ app/src/main/java/com/dev/testgithub/MainActivity.kt
@@ -2,7 +2,7 @@ package com.dev.testgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
